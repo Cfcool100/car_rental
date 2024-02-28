2390058b45d000b71c008a730f1e592538d8e1ba
Cfcool100 <cfcool100@gmail.com>
2024-02-28 13:20:19 +0100

    new shared widget

--- lib/src/core/shared_widget/search_bar.dart
+++ lib/src/core/shared_widget/search_bar.dart
@@ -1,33 +1,46 @@
-
 import 'package:car_rental/src/core/core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppSearchBar extends StatelessWidget {
-  const AppSearchBar({
-    super.key,
-  });
+  const AppSearchBar({super.key, this.onTap, this.prefixIcon, this.onChanged});
+
+  final void Function()? onTap;
+  final Widget? prefixIcon;
+  final void Function(String)? onChanged;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 5.h, right: 5.w),
-      padding: EdgeInsets.symmetric(vertical: 3.h),
+      margin: prefixIcon == null
+          ? EdgeInsets.only(bottom: 1.h, right: 30.w)
+          : EdgeInsets.only(top: 5.h, right: 5.w),
+      padding: EdgeInsets.symmetric(
+          vertical: 3.h, horizontal: prefixIcon != null ? 0 : 18.w),
       decoration: BoxDecoration(
           color: AppColor.white,
-          borderRadius: BorderRadius.circular(10.r)),
+          borderRadius: BorderRadius.circular(10.r),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              offset: const Offset(0, 0),
+              blurRadius: 6,
+              spreadRadius: 1,
+            )
+          ]),
       child: TextField(
+        onChanged: onChanged,
+        onTap: onTap,
+        // enabled: enable,
+        onTapOutside: (event) => FocusScope.of(context).unfocus(),
         decoration: InputDecoration(
           hintText: 'Find your car',
           hintStyle: GoogleFonts.roboto(
-              color: AppColor.santasGray,
-              fontWeight: FontWeight.w400,
-              fontSize: 16.sp),
-          border: InputBorder.none,
-          prefixIcon: const Icon(
-            CupertinoIcons.search,
-            color: AppColor.black,
+            color: AppColor.santasGray,
+            fontWeight: FontWeight.w400,
+            fontSize: 16.sp,
           ),
+          border: InputBorder.none,
+          prefixIcon: prefixIcon,
         ),
       ),
     );
--- lib/src/core/shared_widget/section_title.dart
+++ lib/src/core/shared_widget/section_title.dart
@@ -1,5 +1,7 @@
 import 'package:car_rental/src/core/core.dart';
+import 'package:car_rental/src/feature/details/bloc/products_bloc/products_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SectionTitle extends StatelessWidget {
   const SectionTitle({
@@ -22,22 +24,27 @@ class SectionTitle extends StatelessWidget {
             fontSize: 20.sp,
           ),
         ),
-        Row(
-          children: [
-            Text(
-              'View All ',
-              style: GoogleFonts.roboto(
-                color: AppColor.santasGray,
-                fontWeight: FontWeight.w300,
-                fontSize: 14.sp,
+        GestureDetector(
+          onTap: () {
+            context.read<ProductsBloc>().add(ProductFetchEvent());
+          },
+          child: Row(
+            children: [
+              Text(
+                'View All ',
+                style: GoogleFonts.roboto(
+                  color: AppColor.santasGray,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 14.sp,
+                ),
               ),
-            ),
-            const Icon(
-              Icons.arrow_forward_ios_rounded,
-              size: 14,
-              color: AppColor.santasGray,
-            ),
-          ],
+              // const Icon(
+              //   Icons.arrow_forward_ios_rounded,
+              //   size: 14,
+              //   color: AppColor.santasGray,
+              // ),
+            ],
+          ),
         )
       ],
     );
